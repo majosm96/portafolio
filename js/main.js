33c610a60f554af1adf7eacf94c437d3ae117b32
MariaJoseSolis <majosm96@gmail.com>
2017-10-12 14:09:23 -0600

    Ajustes menu/sections/porjectPage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+//Variables
 var menuOpen = document.getElementById("menu");
 var menuClose = document.getElementById("hideMenu");
+var menuBox = document.getElementById("navigation");
+
+
+//Function toogle Menu
 
 function toggleMenu() {
-  var menuBox = document.getElementById('navigation');
 
   if (menuBox.style.display == "block") {
     // if is menuBox displayed, hide it
@@ -13,5 +17,62 @@ function toggleMenu() {
   }
 }
 
+function closeToggleMenu() {
+  menuBox.style.display == "block";
+}
+
+
+function menuItemsClose() {
+  var menuItems = [].slice.call(document.getElementsByClassName("menuItem"));
+
+  menuItems.forEach(function(element, index) {
+    element.addEventListener("click", function() {
+      menuBox.style.display = "none";
+      console.log("adesd");
+    });
+  });
+}
+
+var project= document.getElementById("projectInfo");
+
+function mouseOver() {  
+  document.getElementById("project-img").style.opacity = "0.0";
+}
+
+function mouseOut() {  
+  document.getElementById("project-img").style.opacity = "0.8";
+}
+
+//Calls Menu
 menuOpen.addEventListener("click", toggleMenu);
 menuClose.addEventListener("click", toggleMenu);
+menuItemsClose();
+
+project.addEventListener("mouseover", mouseOver);
+project.addEventListener("mouseout", mouseOut);
+
+// function hoverBg(img) {
+
+//   var projectItems = [].slice.call(document.getElementsByClassName("project"));
+
+//   projectItems.forEach(function(element, index) {
+
+//     element.addEventListener("mouseover", function() {
+//       img.style.opacity = "0.0";
+//     });
+
+//     element.addEventListener("mouseout", function() {
+//       img.style.opacity = "0.8";
+//     });
+
+//   });
+// }
+
+// var img01 = document.getElementById("project-img01");
+// var img02 = document.getElementById("project-img02");
+// // var img01 = document.getElementById("project-img");
+// // var img01 = document.getElementById("project-img");
+// // var img01 = document.getElementById("project-img");
+
+// hoverBg(img01);
+// hoverBg(img02);
